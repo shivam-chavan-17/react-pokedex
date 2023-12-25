27bd2e820cb18c13de4f77e02d1c933510735926
Shivam Chavan <vc19016@gmail.com>
2023-12-25 23:48:44 +0530

    dropdown

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,87 @@
-import usePokemons from "../Hooks/usePokemons";
-import Pokemonlist from "../components/Pokemonlist";
-import { Button, Container, Grid } from "@mui/material";
-import { IndexedType } from "../interface/pokemon.interface";
+// import usePokemons from "../Hooks/usePokemons";
+// import Pokemonlist from "../components/Pokemonlist";
+// import { Button, Container, Grid } from "@mui/material";
+// import { IndexedType } from "../interface/pokemon.interface";
+
+// const Home = () => {
+//   const {
+//     pokemons,
+//     hasMorePokemon,
+//     fetchNextPage,
+//     pokemonTypes,
+//     setSelectedType,
+//     selectedType,
+//     setPokemons,
+//   } = usePokemons();
+
+//   const handleSelectType = (type: IndexedType | null) => {
+//     if (type) {
+//       setSelectedType(type);
+//     } else {
+//       setPokemons([]);
+//       setSelectedType(null);
+//     }
+//   };
+
+//   return (
+//     <Container>
+//       <Grid container spacing={2} mt={1}>
+//         <Grid
+//           container
+//           item
+//           xs={12}
+//           sx={{ display: "flex", justifyContent: "center" }}
+//         >
+//           {pokemonTypes.map((type) => (
+//             <Button
+//               variant="contained"
+//               sx={{
+//                 "&.MuiButton-contained": {
+//                   background: type.color,
+//                 },
+//                 m: 1,
+//               }}
+//               onClick={() => handleSelectType(type)}
+//             >
+//               {type.name}
+//             </Button>
+//           ))}
+//           <Button
+//             variant="contained"
+//             sx={{
+//               m: 1,
+//             }}
+//             onClick={() => handleSelectType(null)}
+//           >
+//             All
+//           </Button>
+//         </Grid>
+//         <Grid
+//           container
+//           item
+//           xs={12}
+//           sx={{ display: "flex", justifyContent: "center" }}
+//         >
+//           <Pokemonlist pokemons={pokemons}></Pokemonlist>
+//           {hasMorePokemon ? (
+//             <Button variant="contained" onClick={fetchNextPage}>
+//               Load More Pokemon
+//             </Button>
+//           ) : null}
+//         </Grid>
+//       </Grid>
+//     </Container>
+//   );
+// };
+
+// export default Home;
+
+
+import React from 'react';
+import usePokemons from '../Hooks/usePokemons';
+import Pokemonlist from '../components/Pokemonlist';
+import { Button, Container, Grid, Select, MenuItem } from '@mui/material';
+import { IndexedType } from '../interface/pokemon.interface';
 
 const Home = () => {
   const {
@@ -30,37 +110,25 @@ const Home = () => {
           container
           item
           xs={12}
-          sx={{ display: "flex", justifyContent: "center" }}
+          sx={{ display: 'flex', justifyContent: 'flex-end', padding: 2 }}
         >
-          {pokemonTypes.map((type) => (
-            <Button
-              variant="contained"
-              sx={{
-                "&.MuiButton-contained": {
-                  background: type.color,
-                },
-                m: 1,
-              }}
-              onClick={() => handleSelectType(type)}
-            >
-              {type.name}
-            </Button>
-          ))}
-          <Button
-            variant="contained"
-            sx={{
-              m: 1,
-            }}
-            onClick={() => handleSelectType(null)}
+          <Select
+            value={selectedType}
+            onChange={(e) => handleSelectType(e.target.value as IndexedType)}
           >
-            All
-          </Button>
+            <MenuItem value="">All</MenuItem>
+            {pokemonTypes.map((type) => (
+              <MenuItem key={type.name} value={type.name}>
+                {type.name}
+              </MenuItem>
+            ))}
+          </Select>
         </Grid>
         <Grid
           container
           item
           xs={12}
-          sx={{ display: "flex", justifyContent: "center" }}
+          sx={{ display: 'flex', justifyContent: 'center' }}
         >
           <Pokemonlist pokemons={pokemons}></Pokemonlist>
           {hasMorePokemon ? (
@@ -75,3 +143,4 @@ const Home = () => {
 };
 
 export default Home;
+
