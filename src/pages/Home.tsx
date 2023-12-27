32a83dd083f37aea1111710e92f5984cb01805b9
Shivam Chavan <vc19016@gmail.com>
2023-12-28 00:22:07 +0530

    search bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import usePokemons from "../Hooks/usePokemons";
 import Pokemonlist from "../components/Pokemonlist";
-import { Button, Container, Grid, MenuItem, Select } from "@mui/material";
+import {
+  Button,
+  Container,
+  Grid,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import { IndexedType } from "../interface/pokemon.interface";
+import { useState } from "react";
 
 const Home = () => {
   const {
@@ -14,6 +22,8 @@ const Home = () => {
     setPokemons,
   } = usePokemons();
 
+  const [search, setSearch] = useState("");
+
   const handleSelectType = (type: IndexedType | null) => {
     if (type) {
       setSelectedType(type);
@@ -23,32 +33,56 @@ const Home = () => {
     }
   };
 
+  const handleSearch = () => {
+    const filteredPokemons = pokemons.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setPokemons(filteredPokemons);
+  };
+
   return (
     <Container>
       <Grid container spacing={2} mt={1}>
-        <Grid
-          container
-          item
-          xs={12}
-          sx={{ display: "flex", justifyContent: "right" }}
-        >
-          <Select value={selectedType || ""}>
-            {pokemonTypes.map((type) => (
-              <MenuItem onClick={() => handleSelectType(type)}>
-                {type.name}
-              </MenuItem>
-            ))}
-          </Select>
-          {/* <Button
-            variant="contained"
+          <Grid
+            container
+            item
+            xs={12}
+            md={6}
             sx={{
-              m: 1,
+              display: "flex",
+              justifyContent: "left",
+              marginBottom: { xs: 1, md: 0 },
             }}
-            onClick={() => handleSelectType(null)}
           >
-            All
-          </Button> */}
-        </Grid>
+            <TextField
+              label="Search Pokemon"
+              variant="outlined"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <Button variant="contained" onClick={handleSearch}>
+              Search
+            </Button>
+          </Grid>
+          <Grid
+            container
+            item
+            xs={12}
+            md={6}
+            sx={{
+              display: "flex",
+              justifyContent: "right",
+              marginBottom: { xs: 1, md: 0 },
+            }}
+          >
+            <Select value={selectedType || ""}>
+              {pokemonTypes.map((type) => (
+                <MenuItem onClick={() => handleSelectType(type)}>
+                  {type.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </Grid>
         <Grid
           container
           item
